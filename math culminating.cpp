c3dbef95e57611e30619f346706132f221fa3b48
helpmynamewontfi <aydan.jiwani@gmail.com>
2017-07-17 17:00:53 -0400

    Add files via upload
    I actually made this as a math project, hence the na,e

---
+++ math culminating.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+// main() is where program execution begins.
+
+int slots(int input) {
+  int result = rand();
+
+
+  return result;
+}
+
+
+int main() {
+int roll1 = slots(5);
+
+cout << roll1;
+
+
+}
